dbd7af48b59916014bac364c0509308bce9bbe7e
dacosta16 <59155171+dacosta16@users.noreply.github.com>
2020-02-12 00:44:09 -0600

    RestServer-Usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
@@ -9,38 +10,32 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function(req, res) {
+mongoose.connect(process.env.urlDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    },
 
-    let body = req.body;
+    (err, resp) => {
+        if (err) throw err;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        });
+        console.log('Base de Datos ONLINE');
+
     }
-});
 
-app.put('/usuario/:id', function(req, res) {
 
-    let id = req.params.id;
+);
 
-    res.json({
-        id
-    });
-});
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
+/*mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de Datos Online');
+
+});*/
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto: ', process.env.PORT);
